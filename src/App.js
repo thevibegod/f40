b94845d567b650a09d12233c4f4fd4b2496dd137
Adarsh <adarsh@thevibegod.com>
2019-10-26 19:45:31 +0530

    Delinking student portal

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import React,{useState} from 'react';
-import RenderStudentLogin from './Pages/StudentPages/RenderStudentLogin';
-import RenderMentorLogin from './Pages/MentorPages/RenderMentorLogin';
+import React, { useState} from 'react';
+import Login from './Pages/StudentPages/Login';
+import Home from './Pages/StudentPages/Home';
 
+var authVal = false;
 
-function App(){
-  const [pageSetter,setPageSetter] = useState('student')
+export default function App(props){
 
-  if(pageSetter==='student'){
-return(<div>
-  <RenderStudentLogin pageSetter={setPageSetter}/>
-  </div>
-)}
-else{
-  return(<div>
-    <RenderMentorLogin pageSetter={setPageSetter}/>
+  const [user,setUser] = useState(null);
+
+  const submit=(value)=>{
+      setUser(value);
+      authVal = true;
+
+    }
+    const handlePage =() => {
+      props.pageSetter('mentor');
+    }
+    if(user){
+      if(authVal){
+        return (<Home user={user} handleLogin={submit}/>)
+    }
+  }
+
+    return (<div>
+    <Login handleLogin={submit}/>
+    <p onClick={()=>handlePage()}>Mentor Login</p>
     </div>)
 }
-}
-export default App;
