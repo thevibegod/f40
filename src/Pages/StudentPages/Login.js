83504ea6b10b9997164b5b207c0d3aee9bbbc4f1
S Praveen Kumar <praveens.bec@gmail.com>
2019-10-27 17:19:20 +0530

    Fix issue in Navbar(merge conflicts)

--- src/Pages/StudentPages/Login.js
+++ src/Pages/StudentPages/Login.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-//import "./index.css";
-import {Form,Button,Card,Jumbotron} from 'react-bootstrap';
+import { Form, Button, Card, Jumbotron } from "react-bootstrap";
 
 class Login extends Component {
   constructor(props) {
@@ -27,32 +26,50 @@ class Login extends Component {
     return (
       <div>
         <Jumbotron>
-        <center>
-          <div className="container">
-            <h1>Department of Electronics & Communication Engineering</h1>
-            <h2>Kumaraguru College of Technology</h2>
-            <h3>Student Portal - Log in</h3>
-          </div>
+          <center>
+            <div className="container">
+              <h1>Department of Electronics & Communication Engineering</h1>
+              <h2>Kumaraguru College of Technology</h2>
+              <h3>Student Portal - Log in</h3>
+            </div>
           </center>
         </Jumbotron>
         <center>
-        <Card border='primary' style={{ width: '18rem',alignItems:'center' }}>
-        <Card.Body>
-          <Form>
-            <Form.Group controlId="loginformusername">
-              <Form.Label>User Name</Form.Label>
-              <Form.Control type="text" placeholder="User name" onChange={this.handleChange} />
-            </Form.Group>
-            <Form.Group controlId="loginformpassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" onChange={this.handleChange}/>
-            </Form.Group>
-            <Button variant="primary" type="submit" onClick={this.submit} block={true}>
-            Log in
-            </Button>
-          </Form>
-          </Card.Body>
-          </Card></center>
+          <Card
+            shadow
+            border="primary"
+            style={{ width: "18rem", alignItems: "center" }}
+          >
+            <Card.Body>
+              <Form>
+                <Form.Group controlId="loginformusername">
+                  <Form.Label>User Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="User name"
+                    onChange={this.handleChange}
+                  />
+                </Form.Group>
+                <Form.Group controlId="loginformpassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    onChange={this.handleChange}
+                  />
+                </Form.Group>
+                <Button
+                  variant="primary"
+                  type="submit"
+                  onClick={this.submit}
+                  block={true}
+                >
+                  Log in
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        </center>
       </div>
     );
   }
--- src/Pages/StudentPages/Navbar.js
+++ src/Pages/StudentPages/Navbar.js
@@ -10,43 +10,6 @@ export default class Navbar extends React.Component {
     this.props.handleLogin(null);
   };
 
-<<<<<<< HEAD
-render(){
-  return(
-    <div className="container-fluid">
-        <nav className="navbar navbar-inverse">
-
-          <ul className="navbar navbar-nav">
-            <li style={listItemStyles}><Link to={'/'}>Home</Link></li>
-            <li style={listItemStyles}><Link to={`/Profile&${this.props.username}`}>Profile</Link></li>
-            <li style={listItemStyles}><Link to={`/Tasks&${this.props.username}`}>Tasks</Link></li>
-            <li style={listItemStyles}><Link to={`/Assessments&${this.props.username}`}>Assessments</Link></li>
-            <li style={listItemStyles}><Link to={`/Notifications&${this.props.username}`}>Notifications</Link></li>
-            <li style={listItemStyles}><Link to={`/Courses&${this.props.username}`}>Courses</Link></li>
-            <li style={listItemStyles}><button style={{padding:'0',background:'none',border:'none',fontSize: '20px'}} id="logout" onClick={this.killLogin}>Logout</button></li>
-          </ul>
-
-        </nav>
-          </div>
-)}
-
-}
-
-
-const listStyles = {
-  textDecoration:'none',
-  listStyleType:'none',
-  display:'flex',
-  margin:'0',
-  paddingTop:'10px',
-  float:'right'
-}
-
-const listItemStyles = {
-  paddingRight:'20px',
-  fontFamily:"Helvetica",
-  fontSize: '20px',
-=======
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -111,5 +74,4 @@ const listItemStyles = {
       </nav>
     );
   }
->>>>>>> c9573ca7244d21edf645c6271f7c254911a3540f
 }
