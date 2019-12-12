8fb725c67a3003a3aafb46f165f5f4b7843ce25a
S Praveen Kumar <praveens.bec@gmail.com>
2019-12-12 12:30:47 +0530

    Fix Events image loading

--- src/Pages/StudentPages/Events.js
+++ src/Pages/StudentPages/Events.js
@@ -1,48 +1,65 @@
 import React, { useEffect, useState } from "react";
 import InfoItem from "./InfoItem.js";
-import axios from 'axios';
+import axios from "axios";
 import { Row } from "react-bootstrap";
-export default class Events extends React.Component{
+export default class Events extends React.Component {
   constructor(props) {
-  super(props);
-  this.state={data:[],loading:true}
-}
-  componentDidMount=()=> {
-    let dataMap =[];
-    const headers = {
-      'Content-Type':'application/json',
-      'X-Access-Token':this.props.token
-    }
-
-    //fetch operation
-    axios.get('https://cors-anywhere.herokuapp.com/f40-server.adarshfrompupil.now.sh/getallevents',{headers}).then(res=>{
-       res.data.map((value)=>{
-        return fetch(`https://cors-anywhere.herokuapp.com/f40-server.adarshfrompupil.now.sh/eventpicture?title=${value.title}`,{headers}).then(
-          (res)=>{return res.blob()}).then((blob)=>{dataMap.push({uid:value.id,title:value.title,desc:value.desc,imgURL:URL.createObjectURL(blob)})}
-      )
-      })
-    }
-
-  ).then(()=>this.setState({data:dataMap,loading:false})).catch(err=>console.log(err))
-
+    super(props);
+    this.state = { data: [], loading: true };
   }
-render(){
-  if(this.state.loading){
-  return (<p>Loading</p>);
-}else {
-  return(
-    <div className="container">
-      <center><h1>Events</h1></center>
-      {console.log('hi')}
-      <Row style={{ justifyContent: "center" }}>
-        {this.state.data.map(item => {
-          console.log(item)
-          return <InfoItem item={item} key={item.uid} />;
-        })}
-      </Row>
-    </div>
-  );
-}
-
-}
+  componentDidMount = () => {
+    let dataMap = [];
+    const headers = {
+      "Content-Type": "application/json",
+      "X-Access-Token": this.props.token
+    };
+    axios
+      .get(
+        "https://cors-anywhere.herokuapp.com/f40-server.adarshfrompupil.now.sh/getallevents",
+        { headers }
+      )
+      .then(res => {
+        console.log(res);
+        this.setState({ data: res.data, loading: true });
+        return Promise.all(
+          res.data.map(value => {
+            return fetch(
+              `https://cors-anywhere.herokuapp.com/f40-server.adarshfrompupil.now.sh/eventpicture?title=${value.title}`,
+              { headers }
+            );
+          })
+        );
+      })
+      .then(res => {
+        return Promise.all(res.map(value => value.blob()));
+      })
+      .then(res => {
+        dataMap = this.state.data;
+        res.forEach((blob, index) => {
+          dataMap[index].imgURL = URL.createObjectURL(blob);
+        });
+      })
+      .then(() => this.setState({ data: dataMap, loading: false }))
+      .catch(err => console.log(err));
+  };
+  render() {
+    if (this.state.loading) {
+      return <p>Loading</p>;
+    } else {
+      return (
+        <div className="container">
+          <center>
+            <h1>Events</h1>
+          </center>
+          {console.log("hi")}
+          <Row style={{ justifyContent: "center" }}>
+            {this.state.data.map(item => {
+              console.log(item);
+              return <InfoItem item={item} key={item.uid} />;
+            })}
+          </Row>
+        </div>
+      );
+    }
+  }
 }
