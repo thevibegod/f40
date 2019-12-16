fcdb7a98cf6556cb4ed946cdb8914cb7b812c35c
PGOKULNATH <4gokul2000@gmail.com>
2019-12-16 17:35:29 +0530

    node_modules

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React, { useState } from "react";
 import Login from "./Pages/StudentPages/Login";
 import Home from "./Pages/StudentPages/Home";
 
-
-
-export default function App(props) {
+export default function App() {
   const [user, setUser] = useState(null);
   const [token,setToken] = useState('')
 
+  const handleLogin = (user,token)=>{setUser(user);setToken(token)}
+
   if (user) {
-      return <Home user={user} token={token} handleLogin={(user,token)=>{setUser(user);setToken(token)}} />;
+      return <Home user={user} token={token} handleLogin={handleLogin} />;
   }
 
   return (
     <div>
-      <Login handleLogin={(user,token)=>{setUser(user);setToken(token)}} />
+      <Login handleLogin={handleLogin} />
     </div>
   );
 }
